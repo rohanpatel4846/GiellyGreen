372aaaa6adcf5bf4e3ba439aa5ebcc9c7dba6452
rohanpatel4846 <rohanpatel25998@gmail.com>
2022-04-25 16:01:55 +0530

    Get Profile Completed

--- Angular/GiellyGreen/src/app/Components/profile/profile.component.ts
+++ Angular/GiellyGreen/src/app/Components/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { ProfileService } from 'src/app/Services/Profile/profile.service';
 import { SessionManagementService } from 'src/app/Services/SessionManagement/session-management.service';
 import Swal from 'sweetalert2';
 
@@ -14,8 +16,9 @@ export class ProfileComponent implements OnInit {
   menuOpened = false;
   ProfileForm!: FormGroup;
   FullPageLoading = false;
+  ProfileID:any = 0;
   
-  constructor(public router:Router,  public SessionManagement: SessionManagementService, private fb: FormBuilder) { }
+  constructor(private notification: NzNotificationService, public profile: ProfileService, public router:Router,  public SessionManagement: SessionManagementService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.ProfileForm = this.fb.group({
@@ -26,6 +29,8 @@ export class ProfileComponent implements OnInit {
       country: [null],
       defaultVat : [null]
     });
+
+    this.UpdateProfileForm();
   }
 
   logOut(){
@@ -43,6 +48,45 @@ export class ProfileComponent implements OnInit {
   }
 
   submitForm(){
-    console.log(this.ProfileForm);
+    console.log(this.ProfileID);
+  }
+
+  UpdateProfileForm(){
+    this.FullPageLoading = true;
+    this.ProfileID = 0;
+    this.profile.getLastProfile()
+    .subscribe((data:any) => {
+      this.FullPageLoading = false;
+      console.log(data);
+      if(data.Result != null){
+        this.ProfileID = data.Result.id;
+        let vat:number = 0;
+        try{
+          vat = data.Result.DefaultVAT;
+        }
+        catch(ex){}
+        this.ProfileForm.patchValue({
+          companyName: [data.Result.CompanyName],
+          addressLine: [data.Result.AddressLine],
+          city: [data.Result.City],
+          zipCode: [data.Result.ZipCode],
+          country: [data.Result.Country],
+          defaultVat : [vat]
+        });
+      }
+    },
+    (error) => {
+      this.FullPageLoading = false;
+      this.serverErrorNotification(error);
+    });
+  }
+
+  serverErrorNotification(DataString:any): void {
+    console.log(DataString);
+    this.notification.create(
+      'error',
+      'Error From Server!',
+      DataString.message
+    );
   }
 }
